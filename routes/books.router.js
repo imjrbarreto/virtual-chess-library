5cd972d0a6f862ace773282ddf8553cbf09dbee9
imjrbarreto <imjrbarreto@gmail.com>
2024-03-17 22:03:47 -0300

    add book service

--- routes/books.router.js
+++ routes/books.router.js
@@ -1,27 +1,38 @@
 const express = require('express');
+const BookService = require('./../services/book.service');
 
 const router = express.Router();
 
+const service = new BookService();
+
 router.get('/', (req, res) => {
-  res.send('get books');
+  const books = service.find();
+  res.json(books);
 });
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  res.send('get book: ' + id);
+  const book = service.findOne(id);
+  res.send(book);
 });
 
 router.post('/', (req, res) => {
   const body = req.body;
-  res.json(body);
+  const newBook = service.create(body)
+  res.json(newBook);
 });
 
-// router.patch('/:id', (req, res) => {
-  
-// });
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  const bookUpdate = service.update(id, body);
+  res.json(bookUpdate);
+});
 
-// router.delete('/:id', (req, res) => {
-
-// });
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  const bookDelete = service.delete(id);
+  res.json(bookDelete);
+});
 
 module.exports = router;
--- services/book.service.js
+++ services/book.service.js
@@ -1,29 +1,56 @@
+const { faker } = require('@faker-js/faker');
+
 class BookService {
   constructor() {
-
+    this.books = [];
+    this.generate();
   }
 
   generate() {
-
+    const limit = 10;
+    for (let index = 0; index < limit; index++) {
+      this.books.push({
+        id: faker.string.uuid(),
+        name: faker.commerce.productName(),
+        description: faker.commerce.productDescription(),
+        year: faker.date.anytime()
+      });
+    }
   }
 
-  create() {
-
+  create(data) {
+    const newBook = {
+      id: faker.string.uuid(),
+      ...data
+    }
+    this.books.push(newBook);
+    return newBook;
   }
 
   find() {
+    return this.books;
   }
 
-  findOne() {
-
+  findOne(id) {
+    const book = this.books.find(item => item.id === id);
+    // validar...
+    return book;
   }
 
-  update() {
-
+  update(id, changes) {
+    const index = this.books.findIndex(item => item.id === id);
+    const bookUpdate = this.books[index];
+    this.books[index] = {
+      ...bookUpdate,
+      ...changes
+    }
+    return this.books[index];
   }
 
-  delete() {
-
+  delete(id) {
+    const index = this.books.findIndex(item => item.id === id);
+    this.books.splice(index, 1);
+    return { id }
   }
 }
 
